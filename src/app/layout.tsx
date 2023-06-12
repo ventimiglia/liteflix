b3e20df4b9cf78cf2cc6bc88dc9d7a5180c8de77
ventimiglia <sebaventimiglia23@gmail.com>
2023-06-12 20:19:58 -0300

    feat(test): add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import './globals.css'
-import { Bebas_Neue } from 'next/font/google'
+import "./globals.css";
+import { Bebas_Neue } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const bebasNeue = Bebas_Neue({
   weight: ["400"],
   subsets: ["latin"],
-})
+});
 
 export const metadata = {
-  title: 'Liteflix',
-}
+  title: "Liteflix",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={bebasNeue.className}>{children}</body>
     </html>
-  )
+  );
 }
