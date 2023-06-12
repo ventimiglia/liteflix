79855822521e2255ddfd13c3e8b525ff473fd0df
ventimiglia <sebaventimiglia23@gmail.com>
2023-06-12 20:28:36 -0300

    fix(analytics): change rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Analytics />
-      <body className={bebasNeue.className}>{children}</body>
+      <body className={bebasNeue.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
